a3ab66e677159032856e27feb5d2424cf84a5f92
Stefaan <naafetss@gmail.com>
2018-03-09 20:31:33 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase'
+import Vuelidate from 'vuelidate'
+
+Vue.use(Vuelidate)
+
+const config = {
+  // insert firebase config object here
+}
+
+firebase.initializeApp(config)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p class="is-size-4">testing bulma</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
